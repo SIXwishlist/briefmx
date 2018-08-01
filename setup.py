32b7b93e577a0c449cd0741ad5388a745d2d3c3e
Roman Tomjak <r.tomjaks@gmail.com>
2018-08-01 22:38:11 +0100

    add license

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='briefmx',
     version='0.1.0',
     packages=['briefmx'],
-    url='http://github.com/romantomjak/briefmx',
+    url='https://github.com/romantomjak/briefmx',
     license='MIT',
     author='Roman Tomjak',
     author_email='r.tomjaks@gmail.com',
